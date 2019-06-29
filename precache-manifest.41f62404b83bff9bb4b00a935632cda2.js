19a325bbbd905224b88202f499db06d9bddd72aa
jodreclado <rogersx@uw.edu>
2019-06-29 04:04:01 -0700

    Updates

---
+++ precache-manifest.41f62404b83bff9bb4b00a935632cda2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a708ae49f6e4dd525cb3f137bb765aa",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "55fbfcbb503ab97874f7",
+    "url": "/react-tic-tac-toe/static/css/main.2206f90e.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/react-tic-tac-toe/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "55fbfcbb503ab97874f7",
+    "url": "/react-tic-tac-toe/static/js/main.2355f795.chunk.js"
+  },
+  {
+    "revision": "140d55aca7c0751087c1",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.a5f725ed.js"
+  }
+]);
